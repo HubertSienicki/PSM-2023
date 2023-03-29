53ec91db5660f3b0b5f40109b204a01b7d9dd116
HubertSienicki <businesssienicki@gmail.com>
2023-03-29 19:52:41 +0200

    changed some code

--- SymulacjaRuchu/main.py
+++ SymulacjaRuchu/main.py
@@ -1,67 +1,61 @@
 import math
 import matplotlib.pyplot as plt
 
-# Constants
-g = 9.81  # m/s^2
-mass = 1.0  # kg
-drag_coeff = 0.01  # kg/m
+g = 9.81
+mass = 1.0
+drag = 0.01
 
-# Initial conditions
-x0, y0 = 0.0, 0.0  # m
-v0 = 50.0  # m/s
-theta = math.pi / 4  # radians
+start_x, start_y = 0.0, 0.0 
+start_v = 50.0
+theta = math.pi / 4
 
-# Time step and duration
-dt = 0.01  # s
-t_max = 10.0  # s
+dt = 0.01
+t_max = 10.0
 
-# Euler's method
 def euler_method():
-    x, y = x0, y0
-    vx, vy = v0 * math.cos(theta), v0 * math.sin(theta)
+    x, y = start_x, start_y
+    v_xx, v_y = start_v * math.cos(theta), start_v * math.sin(theta)
     x_data, y_data = [x], [y]
 
     while y >= 0.0:
-        v = math.sqrt(vx ** 2 + vy ** 2)
-        Fx = -drag_coeff * v * vx
-        Fy = -mass * g - drag_coeff * v * vy
-        ax = Fx / mass
-        ay = Fy / mass
+        v = math.sqrt(v_xx ** 2 + v_y ** 2)
+        force_x = -drag * v * v_xx
+        force_y = -mass * g - drag * v * v_y
+        acceleration_x = force_x / mass
+        acceleration_y = force_y / mass
 
-        x += vx * dt
-        y += vy * dt
-        vx += ax * dt
-        vy += ay * dt
+        x += v_xx * dt
+        y += v_y * dt
+        v_xx += acceleration_x * dt
+        v_y += acceleration_y * dt
 
         x_data.append(x)
         y_data.append(y)
 
     return x_data, y_data
 
-# Midpoint method
 def midpoint_method():
-    x, y = x0, y0
-    vx, vy = v0 * math.cos(theta), v0 * math.sin(theta)
+    x, y = start_x, start_y
+    v_xx, v_y = start_v * math.cos(theta), start_v * math.sin(theta)
     x_data, y_data = [x], [y]
 
     while y >= 0.0:
-        v = math.sqrt(vx ** 2 + vy ** 2)
-        Fx = -drag_coeff * v * vx
-        Fy = -mass * g - drag_coeff * v * vy
-        ax = Fx / mass
-        ay = Fy / mass
+        v = math.sqrt(v_xx ** 2 + v_y ** 2)
+        force_x = -drag * v * v_xx
+        force_y = -mass * g - drag * v * v_y
+        acceleration_x = force_x / mass
+        acceleration_y = force_y / mass
 
-        x += vx * dt + 0.5 * ax * dt ** 2
-        y += vy * dt + 0.5 * ay * dt ** 2
-        vx += ax * dt
-        vy += ay * dt
+        x += v_xx * dt + 0.5 * acceleration_x * dt ** 2
+        y += v_y * dt + 0.5 * acceleration_y * dt ** 2
+        v_xx += acceleration_x * dt
+        v_y += acceleration_y * dt
 
         x_data.append(x)
         y_data.append(y)
 
     return x_data, y_data
 
-# Visualize the trajectory using Matplotlib
 euler_x, euler_y = euler_method()
 midpoint_x, midpoint_y = midpoint_method()
 
